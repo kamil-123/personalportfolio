4a0764947b288a9ee0d5ec153bae53e6bc0fa28e
Kamil Drozd <drozdk@centrum.cz>
2020-02-18 23:56:27 +0100

    added some simple javascript for css transitions

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+// console.log('It works');
+document.addEventListener('DOMContentLoaded',()=>{
+  const hometitle1 = document.querySelector('#hometitle1');
+  hometitle1.classList.remove('hometitle1--right');
+  const hometitle2 = document.querySelector('#hometitle2');
+  hometitle2.classList.remove('hometitle2--left');
+  const hometitle3 = document.querySelector('#hometitle3');
+  hometitle3.classList.remove('hometitle3--appear');
+  const motto = document.querySelector('#motto');
+  motto.classList.remove('motto--appear');
+
+  
+});
